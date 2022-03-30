fc571c4cdb57519c1b7f97dfe50800ab805bd274
antongomez <anton.gomez.lopez@rai.usc.es>
2022-03-30 16:34:34 +0200

    Buscador acabado

--- busquedaActividades.js
+++ busquedaActividades.js
@@ -8,13 +8,38 @@ function buscar(){
       }
     });
 
+    var taboa = '';
     $(actividadesFiltradas).each(function(){
-        
+      taboa += '<tr> <td>' + this.TipoActividade + '</td> <td>' + this.Data + '</td>'
+            + '<td><a href="' + this.Enlace + '">' + this.Titulo + '</a> </td>'
+            + '<td>' + this.Tempo + '</td> <td>' + this.Distancia + '</td> <td></td>'
+            + '<td><a href="">Editar</a></td> + <td><a href="">Eliminar</a></td> </tr>\n';
+
     });
 
+    var encabezado = $("table tr").has("th");
+    $("table tr").not(encabezado).remove();
+    encabezado.after(taboa);
   });
 }
 
 $(document).ready(function(){
   $("#boton_buscar").on('click', buscar);
+  $("#txt_tipo").val("todo");
+  $("#txt_buscador").val("");
+  buscar();
 });
+
+
+// <tr class="odd">
+//   <td>Carreira</td>
+//   <td>mar.,07/03/2022</td>
+//   <td>
+//     <a href="html_Actividades\antongomezlopez\actividade_anton_1.html">Carreira de mañá</a>
+//   </td>
+//   <td>36:00</td>
+//   <td>9,00 km</td>
+//   <td></td>
+//   <td><a href="">Editar</a></td>
+//   <td><a href="">Eliminar</a></td>
+// </tr>
