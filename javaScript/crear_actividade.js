092c4774a299d5eceb39a82b1c8949178b1d05cd
antongomez <anton.gomez.lopez@rai.usc.es>
2022-03-30 18:27:25 +0200

    Empezando cargar actividades en perfil

--- javaScript/crear_actividade.js
+++ javaScript/crear_actividade.js
@@ -1,40 +1,42 @@
-/*function engadirActividade() {
-
-}*/
-// Facer esta funcion en perfil igual que en principal e despois
-// aqui comporbar que todos os campos estan ben e redireccionar a perfil
-// e ali meter a actividade a pelo. Polo tanto isto xa non e json
-// polo que podo mirar de meter algo de algun evento distinto ou
-// jQuery ou javascript 6
-
 
 function comprobarCampos(){
   var error = false;
-  $(".campo_obligatorio").each(function(){
-    var texto = $(this).val().trim();
-    $(this).removeClass("borde_rojo");
-    if (texto.length == 0){
-      error = true;
-      $(this).addClass("borde_rojo");
-    }
+
+  let prom = new Promise(function(crearActividade, mostrarErro){
+
+    $(".campo_obligatorio").each(function(){
+      var texto = $(this).val().trim();
+      $(this).removeClass("borde_rojo");
+      if (texto.length == 0){
+        error = true;
+        $(this).addClass("borde_rojo");
+      }
+    });
+
+      if(error === true){
+        mostrarErro();
+      } else {
+        crearActividade();
+      }
   });
-  if (error == true){
-    $("#mensaje_error").removeClass("texto_oculto");
-    $(document).scrollTop("0");
-  }
-  else{
-    $("#mensaje_error").addClass("texto_oculto");
-    //Programar la creacion de la actividad
 
-
-  }
-
+  prom.then(
+    function() {
+      $("#mensaje_error").addClass("texto_oculto");
+      var actividade = new Object();
+    //  actividade.distacia = $("#distancia").val();
+      //console.log(actividade.distancia);
+      //  console.log($("#distancia").val());
+    },
+    function(){
+      $("#mensaje_error").removeClass("texto_oculto");
+      $(document).scrollTop("0");
+    }
+  );
 
 }
 
 $(document).ready(function(){
   $("#mensaje_error").addClass("texto_oculto");
   $("#btn-crear").on('click', comprobarCampos);
-
-
 });
