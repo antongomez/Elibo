9364df0e6e068b8d3c841db9324152a31dda855e
antongomez <anton.gomez.lopez@rai.usc.es>
2022-03-30 13:25:05 +0200

    Buscador semifuncionando

--- busquedaActividades.js
+++ busquedaActividades.js
@@ -2,14 +2,14 @@ function buscar(){
   $.post('baseDatosActividadesAnton.json', function(datos){
     var actividadesFiltradas = $(datos.Actividades).filter(function(idx) {
       if($("#txt_tipo").val() === "todo"){
-        return datos.Actividades[idx].Titulo === $("#txt_buscador").val();
+        return datos.Actividades[idx].Titulo.includes($("#txt_buscador").val());
       } else {
-        return datos.Actividades[idx].TipoActividade === $("#txt_tipo").val();
+        return datos.Actividades[idx].TipoActividade === $("#txt_tipo").val() && datos.Actividades[idx].Titulo.includes($("#txt_buscador").val());
       }
     });
 
     $(actividadesFiltradas).each(function(){
-        console.log(this);
+        
     });
 
   });
